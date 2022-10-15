6f1edcf0684e4a606279a4169b73c3ff04644819
kodzyz <kodzyzdok@gmail.com>
2022-10-16 00:11:15 +0300

    csv writer

--- lesson_2/client.py
+++ lesson_2/client.py
@@ -1,6 +1,19 @@
 # python3 client.py
 import csv
 
+data = [['hostname', 'vendor', 'model', 'location'],
+        ['kp1', 'Cisco', '2960', 'Moscow, str'],
+        ['kp2', 'Cisco', '2960', 'Novosoborsk, str'],
+        ['kp3', 'Cisco', '2960', 'Kazan, str'],
+        ['kp4', 'Cisco', '2960', 'Tomsk, str']]
 
+with open('new_data.csv', 'w') as f:
+    f_writer = csv.writer(f)
+    for row in data:
+        f_writer.writerow(row)
 
-
+# hostname,vendor,model,location
+# kp1,Cisco,2960,"Moscow, str"
+# kp2,Cisco,2960,"Novosoborsk, str"
+# kp3,Cisco,2960,"Kazan, str"
+# kp4,Cisco,2960,"Tomsk, str"
