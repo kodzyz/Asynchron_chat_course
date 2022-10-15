fb12e712dcf537da3500e2a88ec7f544ae8492b0
kodzyz <kodzyzdok@gmail.com>
2022-10-16 00:52:32 +0300

    json.loads

--- lesson_2/client.py
+++ lesson_2/client.py
@@ -2,15 +2,7 @@
 import json
 
 with open('data.json') as f:
-    objects = json.load(f)
-    print('from obj: ', objects['action'])  # значение по ключу
-    for s, c in objects.items():
-        print(s, c)  # КЛЮЧ: ЗНАЧЕНИЕ
-
-# from obj:  msg
-# action msg
-# time <unix timestamp>
-# to account_name
-# from account_name
-# encoding ascii
-# message message
+    objects = json.loads(f.read())
+    print(objects, type(objects))
+    # {'action': 'msg', 'time': '<unix timestamp>', 'to': 'account_name', 'from': 'account_name', 'encoding': 'ascii', 'message': 'message'} <class 'dict'>
+    print('data', objects['action'])  # data msg
