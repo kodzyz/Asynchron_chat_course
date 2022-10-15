741d09834cce2b5f76fbe17a3f0d615379b06e93
kodzyz <kodzyzdok@gmail.com>
2022-10-15 23:28:32 +0300

    параметр delimiter

--- lesson_2/client.py
+++ lesson_2/client.py
@@ -2,12 +2,14 @@
 import csv
 
 with open('data.csv') as f:
-    f_reader = csv.reader(f)
+    #возможность изменения разделителя (if default не запятая)
+    f_reader = csv.reader(f, delimiter="!")
     next(f_reader)
     for l in f_reader:
         print(l)
 
-# ['kp1', 'Cisco', '2960', 'Moscow']
-# ['kp2', 'Cisco', '2960', 'Novosoborsk']
-# ['kp3', 'Cisco', '2960', 'Kazan']
-# ['kp4', 'Cisco', '2960', 'Tomsk']
+# ['kp1,Cisco,2960,Moscow']
+# ['kp2,Cisco,2960,Novosoborsk']
+# ['kp3,Cisco,2960,Kazan']
+# ['kp4,Cisco,2960,Tomsk']
+
