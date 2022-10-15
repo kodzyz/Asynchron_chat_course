647c1bb3ffc892f9fc5a3249a3b219350284d421
kodzyz <kodzyzdok@gmail.com>
2022-10-15 23:34:08 +0300

    DictReader

--- lesson_2/client.py
+++ lesson_2/client.py
@@ -2,14 +2,21 @@
 import csv
 
 with open('data.csv') as f:
-    #возможность изменения разделителя (if default не запятая)
-    f_reader = csv.reader(f, delimiter="!")
-    next(f_reader)
-    for l in f_reader:
-        print(l)
+    f_reader = csv.DictReader(f)  # должен быть headers
+    for row in f_reader:
+        print(row)
 
-# ['kp1,Cisco,2960,Moscow']
-# ['kp2,Cisco,2960,Novosoborsk']
-# ['kp3,Cisco,2960,Kazan']
-# ['kp4,Cisco,2960,Tomsk']
+#-csv
+# hostname,vendor,model,location
+# kp1,Cisco,2960,Moscow
+# kp2,Cisco,2960,Novosoborsk
+# kp3,Cisco,2960,Kazan
+#kp4,Cisco,2960,Tomsk
+
+#-DictReader
+# {'hostname': 'kp1', 'vendor': 'Cisco', 'model': '2960', 'location': 'Moscow'}
+# {'hostname': 'kp2', 'vendor': 'Cisco', 'model': '2960', 'location': 'Novosoborsk'}
+# {'hostname': 'kp3', 'vendor': 'Cisco', 'model': '2960', 'location': 'Kazan'}
+# {'hostname': 'kp4', 'vendor': 'Cisco', 'model': '2960', 'location': 'Tomsk'}
+
 
