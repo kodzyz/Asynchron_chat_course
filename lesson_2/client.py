555897ebd0447c0f334f2d5b45686520e3acfabc
kodzyz <kodzyzdok@gmail.com>
2022-10-16 01:08:04 +0300

    json.dump

--- lesson_2/client.py
+++ lesson_2/client.py
@@ -1,1 +1,13 @@
 # python3 client.py
+import json
+
+data = {
+    "action": "msg",
+    "to": "account_name",
+    "from": "account_name",
+    "encoding": "ascii",
+    "message": "message"
+}
+
+with open('new_data.json', 'w') as f:
+    json.dump(data, f)
