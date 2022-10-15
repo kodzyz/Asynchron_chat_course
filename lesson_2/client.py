1f87470b10d55b8497e6a46fee299fc6b0b4acb9
kodzyz <kodzyzdok@gmail.com>
2022-10-16 00:54:08 +0300

    0:38 json запись

--- lesson_2/client.py
+++ lesson_2/client.py
@@ -1,8 +1,1 @@
 # python3 client.py
-import json
-
-with open('data.json') as f:
-    objects = json.loads(f.read())
-    print(objects, type(objects))
-    # {'action': 'msg', 'time': '<unix timestamp>', 'to': 'account_name', 'from': 'account_name', 'encoding': 'ascii', 'message': 'message'} <class 'dict'>
-    print('data', objects['action'])  # data msg
