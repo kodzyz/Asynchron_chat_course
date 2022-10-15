03c2800bf31e274eb391cf7ec21f619a4f738a7f
kodzyz <kodzyzdok@gmail.com>
2022-10-16 02:01:10 +0300

    yaml.safe_load 0:53

--- lesson_2/client.py
+++ lesson_2/client.py
@@ -1,2 +1,12 @@
 # python3 client.py
+# virtualenv venv
+# source venv/bin/activate
+# pip install pyyaml
 
+import yaml
+
+with open('data.yaml') as f:
+    f_content = yaml.safe_load(f)
+    print(f_content)
+
+# {'message': ['msg_1', 'msg_2', 'msg_3'], 'to': ['account_1', 'account_2', 'account_3']}
