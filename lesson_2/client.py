e0e8d428f4b8589e5846f1064bb92ac3c2805668
kodzyz <kodzyzdok@gmail.com>
2022-10-16 16:53:24 +0300

    создать yaml.dump

--- lesson_2/client.py
+++ lesson_2/client.py
@@ -5,3 +5,10 @@
 
 import yaml
 
+action_list = ['msg1', 'msg2', 'msg3']
+to_list = ['account1', 'account2', 'account3']
+
+data_to_uml = {'action': action_list, 'to': to_list}
+
+with open('new_data.yaml', 'w') as f:
+    yaml.dump(data_to_uml, f, default_flow_style=True)
