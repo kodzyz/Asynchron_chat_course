c0a307d464a3c4cdde3d2526ae4ebe7c447995ad
kodzyz <kodzyzdok@gmail.com>
2022-10-16 16:44:31 +0300

    0:53

--- lesson_2/client.py
+++ lesson_2/client.py
@@ -5,8 +5,3 @@
 
 import yaml
 
-with open('data.yaml') as f:
-    f_content = yaml.safe_load(f)
-    print(f_content)
-
-# {'message': ['msg_1', 'msg_2', 'msg_3'], 'to': ['account_1', 'account_2', 'account_3']}
