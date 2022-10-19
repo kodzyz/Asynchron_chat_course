aafda4c3ab9f0fa105f8018032c5e35f6a593321
kodzyz <kodzyzdok@gmail.com>
2022-10-19 23:48:28 +0300

    clear

--- server.py
+++ server.py
@@ -1,2 +1,23 @@
 from socket import *
 import time
+
+
+# создаем соединение и ждем соединения от клиента
+def main():
+    s = socket(AF_INET, SOCK_STREAM)
+    s.bind(('localhost', 10000))
+    s.listen(5)
+
+    while True:
+        client, addr = s.accept()
+        print('Получаем запрос на соединение:', addr)
+        timestr = time.ctime(time.time()) + '\n'
+        client.send(timestr.encode('utf-8'))
+        client.close()
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e)
