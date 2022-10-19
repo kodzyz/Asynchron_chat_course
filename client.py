189cbe2d77222a4e58a82aa2e6cc8d4fcdab7281
kodzyz <kodzyzdok@gmail.com>
2022-10-20 00:13:48 +0300

    в виде функции - правильное оформление (0:40)

--- client.py
+++ client.py
@@ -1,8 +1,25 @@
 from socket import *
 
-# инициализация сокета
-s = socket(AF_INET, SOCK_STREAM)
-s.connect(('localhost', 10000))  # подключаемся
-tm = s.recv(1024)  # получаем от сервера сообщение и количество байт для получения данных
-s.close()  # закрываем связь с сервером
-print("Текущее время: ", tm.decode('utf-8'))  # выводим принятое от сервера сообщение
+
+def main():
+    s = socket(AF_INET, SOCK_STREAM)
+    s.connect(('localhost', 10000))
+    tm = s.recv(1024)
+    s.close()
+    print("Текущее время: ", tm.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except Exception as e:
+        print(e)
+
+# (venv) dk@dk-ThinkPad-Edge-E540:~/PycharmProjects/Asynchron_chat/chat_lesson$ python3 client.py
+# Текущее время:  Wed Oct 19 23:52:10 2022
+#
+# (venv) dk@dk-ThinkPad-Edge-E540:~/PycharmProjects/Asynchron_chat/chat_lesson$ python3 client.py
+# Текущее время:  Wed Oct 19 23:52:13 2022
+#
+# (venv) dk@dk-ThinkPad-Edge-E540:~/PycharmProjects/Asynchron_chat/chat_lesson$ python3 client.py
+# Текущее время:  Wed Oct 19 23:52:22 2022
