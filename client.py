07541522412d51a9e6d80958006802acd1842bc1
kodzyz <kodzyzdok@gmail.com>
2022-10-20 23:03:07 +0300

    отправка данных на сервер (0:50)

--- client.py
+++ client.py
@@ -4,9 +4,11 @@ from socket import *
 def main():
     s = socket(AF_INET, SOCK_STREAM)
     s.connect(('localhost', 10000))
-    tm = s.recv(1024)
+    msg = 'Привет, сервер'.encode('utf-8')
+    s.send(msg)  # отправка данных на сервер
+    tm = s.recv(1024)  # получение от сервера
     s.close()
-    print("Текущее время: ", tm.decode('utf-8'))
+    print("Сообщение от сервера: ", tm.decode('utf-8'))
 
 
 if __name__ == '__main__':
@@ -15,11 +17,4 @@ if __name__ == '__main__':
     except Exception as e:
         print(e)
 
-# (venv) dk@dk-ThinkPad-Edge-E540:~/PycharmProjects/Asynchron_chat/chat_lesson$ python3 client.py
-# Текущее время:  Wed Oct 19 23:52:10 2022
-#
-# (venv) dk@dk-ThinkPad-Edge-E540:~/PycharmProjects/Asynchron_chat/chat_lesson$ python3 client.py
-# Текущее время:  Wed Oct 19 23:52:13 2022
-#
-# (venv) dk@dk-ThinkPad-Edge-E540:~/PycharmProjects/Asynchron_chat/chat_lesson$ python3 client.py
-# Текущее время:  Wed Oct 19 23:52:22 2022
+# Сообщение от сервера:  Привет, клиент
