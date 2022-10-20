0e5631c95132124d7c3118ca473271a9b0258871
kodzyz <kodzyzdok@gmail.com>
2022-10-21 00:33:42 +0300

    клиент-сервер if decoded_data == 'close': client.close() (1:06)

--- server.py
+++ server.py
@@ -14,7 +14,11 @@ def main():
     while True:
         # получаем данные от клиента
         data = client.recv(100000)  # ожидаем получение байт данных
-        print('Было получено сообщение от клиента: ', data.decode('utf-8'))
+        decoded_data = data.decode('utf-8')
+        print('Было получено сообщение от клиента: ', decoded_data)
+        if decoded_data == 'close':
+            client.close()
+            return  # потому что нет смысла что то отправлять
         msg = 'Я сервер: Ваше сообщение получено'
         client.send(msg.encode('utf-8'))
 
@@ -25,6 +29,6 @@ if __name__ == '__main__':
     except Exception as e:
         print(e)
 
-# Получаем запрос на соединение: ('127.0.0.1', 42836)
-# Было получено сообщение от клиента:  hi
-# Было получено сообщение от клиента:  hi hi
+# Получаем запрос на соединение: ('127.0.0.1', 50718)
+# Было получено сообщение от клиента:  close
+# (venv) dk@dk-ThinkPad-Edge-E540:~/PycharmProjects/Asynchron_chat/chat_lesson$
