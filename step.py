442b2c320c143a3df6318fbcbee50ee65ccf8ab3
kodzyz <kodzyzdok@gmail.com>
2022-10-23 01:21:43 +0300

    несколько значений параметров python3 step.py -n Вася Оля Петя

--- step.py
+++ step.py
@@ -4,12 +4,12 @@ import argparse
 
 # python3 step.py
 # python3 step.py -n Вася
-# python3 step.py --name Вася
-# python3 step.py --username Вася
+# python3 step.py -n Вася Оля Петя
+
 
 def createParser():
     parser = argparse.ArgumentParser()  # экземпляр класса ArgumentParser
-    parser.add_argument('-n', '--name', '--username')
+    parser.add_argument('-n', '--name', nargs='+', default=['мир'])  # nargs='+', ожидаем одно или более значение
     return parser
 
 
@@ -18,7 +18,16 @@ if __name__ == '__main__':
     namespace = parser.parse_args(sys.argv[1:])  # метод 'parse_args' для разбора командной строки
 
     print(namespace)
-    # Namespace(name=None)
-    # Namespace(name='Вася')
+    # Namespace(name=['мир'])
+    # Namespace(name=['Вася'])
+    # Namespace(name=['Вася', 'Оля', 'Петя'])
 
-    print(f'Привет, {namespace.name}!')  # Привет, None! # Привет, Вася!
+    for name in namespace.name:
+        print(f'Привет, {name}!')
+
+        # Привет, мир! # Привет, Вася!
+        # Привет, Вася!
+        # Привет, Оля!
+        # Привет, Петя!
+
+
