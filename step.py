75d6f25eca9d8dfd8f863e3eda44d242fd3b91a0
kodzyz <kodzyzdok@gmail.com>
2022-10-23 00:43:36 +0300

    параметр 'default' в методе 'add_argument'

--- step.py
+++ step.py
@@ -1,20 +1,21 @@
 import sys
 import argparse
 
+# python3 step.py #  python3 step.py kostya
 
 def createParser():
     parser = argparse.ArgumentParser()  # экземпляр класса ArgumentParser
-    parser.add_argument('name', nargs='?')  # добавили ожидаемый параметр в командной строке (nargs='?')- необязательный
+    parser.add_argument('name', nargs='?', default='мир')  # добавили ожидаемый параметр в командной строке (nargs='?')- необязательный
     return parser
 
 
 if __name__ == '__main__':
     parser = createParser()
-    namespace = parser.parse_args()  # метод 'parse_args' для разбора командной строки
+    namespace = parser.parse_args(sys.argv[1:])  # метод 'parse_args' для разбора командной строки
 
-    print(namespace)  # Namespace(name=None) # Namespace(name='kostya')
+    print(namespace)  # Namespace(name='мир') # Namespace(name='kostya')
 
 if namespace.name:
-    print(f'Привет, {namespace.name}!')
-else:
-    print('Привет, мир!')
+    print(f'Привет, {namespace.name}!')  # Привет, мир! # Привет, kostya!
+
+
