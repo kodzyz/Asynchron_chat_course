4c8a58341c693030c25663bbe80d1223c713c059
kodzyz <kodzyzdok@gmail.com>
2022-11-08 22:31:25 +0300

    filename

--- test.py
+++ test.py
@@ -5,18 +5,12 @@ import logging
 # базовая настройка
 
 # запись в один журнал
-logging.basicConfig(level=logging.DEBUG)  # настройка для всех логеров уровня 10
+logging.basicConfig(level=logging.DEBUG, filename="app.log")  # запись в файл
 
-# создает логер глобально на уровне библиотеки
+# вызываем вывод сообщения дефолтного логгера
 logging.info('info')  # 20
 logging.critical('critical')  # 50
-# ----- сообщения INFO, CRITICAL видим:
-# INFO:root:info
-# CRITICAL:root:critical
 
-logger = logging.getLogger()  # по умолчанию 'root'
-# logger.setLevel(logging.CRITICAL)  # тут меняем уровень с 10 на 50
-
-logger.info('hi I\'m message')  # и пытаемся записать сообщение с уровенем 20 -> и не видим все что ниже
-# ------ без 18 строчки
-# INFO:root:hi I'm message
+log = logging.getLogger("app")  # создаем свой логгер
+log.setLevel(logging.DEBUG)  # уст. уровень
+log.info('Hello world from app logger')  # записываем сообщение от него
