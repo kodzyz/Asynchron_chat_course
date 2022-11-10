f96d173980f6eed398be66ad1ea018511fec404c
kodzyz <kodzyzdok@gmail.com>
2022-11-10 20:20:57 +0300

    handler локальные переменные для вывода данных

--- service.py
+++ service.py
@@ -3,15 +3,23 @@
 import logging
 
 # глобальные переменные в логере для вывода данных
-logging.basicConfig(filename="module.log",
-                    filemode='w', format='%(levelname)-10s %(module)s -- %(asctime)s:  %(message)s')
+logging.basicConfig(format='%(levelname)-10s %(module)s -- %(asctime)s:  %(message)s')
 
 import functions  # после глоб.настр чтобы они успели примениться
 
-log = logging.getLogger('app1')
+log = logging.getLogger('amm')
+
+# настройка handler
+handler = logging.FileHandler('amm.log')
+# изменение переменных вывода у определенного логера
+formater = logging.Formatter('%(levelname)-10s %(message)s')
+handler.setFormatter(formater)
+
+log.addHandler(handler)
+
 log.critical("Error in service")
 
-# ---- терминал когда нет строки 5
-# faced error in functions module
-# Error in service
+
+
+
 
