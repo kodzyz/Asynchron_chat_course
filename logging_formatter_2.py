6abf858c016a9e676d5da38f212f48134307b479
kodzyz <kodzyzdok@gmail.com>
2022-11-10 22:00:40 +0300

    коды примеров: -Объекты класса Handler -Форматирование сообщений

---
+++ logging_formatter_2.py
@@ -0,0 +1,35 @@
+"""
+Журналирование (логгирование) с использованием модуля logging
+Расширенная настройка. Форматирование, обработчики -с.9
+"""
+
+import sys
+import logging
+
+# Создать логгер - регистратор верхнего уроовня
+# с именем basic
+LOG = logging.getLogger('basic')
+
+# Создать обработчик, который выводит сообщения в поток stderr
+# обработчики позволяют переопределить поведение корневого регистратора - log
+CRIT_HAND = logging.StreamHandler(sys.stderr)
+# выводит в поток сообщения с уровнем CRITICAL
+#CRIT_HAND.setLevel(logging.DEBUG)
+
+# Создать объект Formatter
+# Определить формат сообщений
+FORMATTER = logging.Formatter("%(levelname)-10s %(asctime)s %(message)s")
+
+# подключить объект Formatter к обработчику
+CRIT_HAND.setFormatter(FORMATTER)
+
+# Добавить обработчик к регистратору
+LOG.addHandler(CRIT_HAND)
+LOG.setLevel(logging.DEBUG)
+
+# Передать сообщение обработчику
+LOG.info('Информационное сообщение')
+
+# ------терминал
+# INFO       2022-11-10 21:55:58,727 Информационное сообщение
+
