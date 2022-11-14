861ec8f431ee6778167e941fbcb92a7c9c3648e8
kodzyz <kodzyzdok@gmail.com>
2022-11-14 08:08:43 +0300

    2. декоратор с параметром

--- service.py
+++ service.py
@@ -1,11 +1,17 @@
 # 0:37 декоратор с параметром
+# (с передачей внутренних параметров функции декоратору wrapper )
 
 # python3 service.py
+
+from functools import wraps
+
+
 class Log:
     def __int__(self):
         pass
 
     def __call__(self, func):  # __call__ срабатывает при вызове класса
+        @wraps(func)
         def wrapper(*args, **kwargs):
             print(f'Вызов {func.__name__} {args} {kwargs}')
             # ... до выполнения основной ф-и
@@ -13,6 +19,9 @@ class Log:
             # ... после выполнения основной ф-и
             print(f'{func.__name__} вернула {r}')
             return r
+
+        # wrapper.__doc__ = func.__doc__
+        print('doctest wrapper', wrapper.__doc__)
 
         return wrapper
 
@@ -28,9 +37,11 @@ def square(x):
 
 
 square(4)
-print(square.__doc__)
 
-# Идет выполнение задачи...
-#  Вычисляет квадрат
+# doctest wrapper  Вычисляет квадрат
 #         >>> square(4):
 #         16
+#
+# Вызов square (4,) {}
+# Идет выполнение задачи...
+# square вернула 16
