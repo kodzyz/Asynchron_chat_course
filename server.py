2a09847fdb7ae502fd6a1e104555561fad62c2b1
kodzyz <kodzyzdok@gmail.com>
2022-11-23 22:25:19 +0300

    обычная блокировка процесса

--- server.py
+++ server.py
@@ -1,3 +1,20 @@
 from threading import Thread, Lock
+import time
 
+# показано что блокировки не привязаны к конкретному потоку
+# и мы блоктровку приобретенную в одном процессе отпускаем в другом
+
+done = Lock()  # общая блокировка
+
+
+def idle_release():
+    print("Running release!")
+    time.sleep(5)
+    done.release()  # отпускаем блокировку которая приобреталась в основном потоке
+
+
+done.acquire()  # приобретаем блокировку в основном потоке
+Thread(target=idle_release).start()  # запустили 'idle_release'
+done.acquire()  # приобретаем блокировку снова
+print("Странное поведение мьютексов в Python...")
 
