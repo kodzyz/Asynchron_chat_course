99aeec64fca47e80b2ce24e940a2b6632c6cbddd
kodzyz <kodzyzdok@gmail.com>
2022-11-24 00:03:00 +0300

    очередь и два потока

--- server.py
+++ server.py
@@ -3,14 +3,17 @@
 """
 
 from threading import Thread
-from queue import Queue    
+from queue import Queue
+
+input_queue = Queue()
 
 
 class WorkerThread(Thread):
     """Поток с очередью в классе"""
-    def __init__(self, *args, **kwargs):  # обязательная ф-я
+
+    def __init__(self, queue, *args, **kwargs):  # обязательная ф-я
         super().__init__(*args, **kwargs)
-        self.input_queue = Queue()  # объявление очереди: две функции - положить и забрать
+        self.input_queue = queue  # объявление очереди: две функции - положить и забрать
 
     def send(self, item):
         """Положить объект в очередь"""
@@ -21,14 +24,14 @@ class WorkerThread(Thread):
         self.input_queue.put(None)  # положить = None, очередь close
         self.input_queue.join()  # блокирует соединение, пока все элементы в очереди не будут получены и обработаны
 
-    def run(self):  # WT_OBJ.start()
+    def run(self):  # WT_OBJ.start(), run() - постоянно работает
         while True:
             item = self.input_queue.get()  # метод получает сообщение из очереди, потом удаляет
             if item is None:
                 break
             # Обработать элемент
             # (замените инструкцию print какими-нибудь полезными операциями)
-            print(item)  # если сообщение есть - печатаем
+            print(item, '"read from thread"')  # если сообщение есть - печатаем
             self.input_queue.task_done()  # сообщает очереди, что обработка задачи завершена
         # Конец. Сообщить, что сигнальная метка была принята, и выйти
         self.input_queue.task_done()
@@ -36,9 +39,16 @@ class WorkerThread(Thread):
 
 
 # Пример использования
-WT_OBJ = WorkerThread()
-WT_OBJ.start()
+WT_OBJ_1 = WorkerThread(input_queue)
+WT_OBJ_2 = WorkerThread(input_queue)
+WT_OBJ_1.start()
+WT_OBJ_2.start()
 # Отправить элемент на обработку (с помощью очереди)
-WT_OBJ.send("hello")
-WT_OBJ.send("world")
-WT_OBJ.close()
+# WT_OBJ_1.send("hello w1")
+# WT_OBJ_2.send("world w2")
+input_queue.put('hello threads')
+
+WT_OBJ_1.close()
+WT_OBJ_2.close()
+
+# hello threads "read from thread"
