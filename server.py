6c9f2863648e08b0f65f8123659a3e97e926b111
kodzyz <kodzyzdok@gmail.com>
2022-11-24 19:44:59 +0300

    multiprocessing

--- server.py
+++ server.py
@@ -1,1 +1,25 @@
+"""Класс-процесс"""
 
+import time
+from multiprocessing import Process
+
+
+class ClockProcess(Process):
+    """Простой класс-процесс"""
+    def __init__(self, interval):
+        super().__init__()
+        self.interval = interval
+
+    def run(self):
+        while True:
+            print(f"Текущее время: {time.ctime()}")
+            time.sleep(self.interval)
+
+
+if __name__ == "__main__":
+    PROC = ClockProcess(1)
+    PROC.start()
+
+# Текущее время: Thu Nov 24 00:06:55 2022
+# Текущее время: Thu Nov 24 00:06:56 2022
+# Текущее время: Thu Nov 24 00:06:57 2022
