e04bfbd0ccc0ee8f0cf01780f74681b001e3a057
kodzyz <kodzyzdok@gmail.com>
2022-11-26 23:38:12 +0300

    Модуль pprint

--- service.py
+++ service.py
@@ -1,1 +1,20 @@
+# Модуль pprint # Улучшает качество отображения Python-объектов
+from pprint import pprint
 
+# Отображение словаря с вложенными словарями
+dict_dicts = {'el_1': {'el_1.1': 'val_1.1', 'el_1.2': 'val_1.2', 'el_1.3': 'val_1.3'},
+              'el_2': {'el_2.1': 'val_2.1', 'el_2.2': 'val_2.2', 'el_2.3': 'val_2.3'},
+              'el_3': {'el_3.1': 'val_3.1', 'el_3.2': 'val_3.2', 'el_3.3': 'val_3.3'}}
+pprint(dict_dicts)  # вместо вывода строки — упорядоченное представление
+# {'el_1': {'el_1.1': 'val_1.1', 'el_1.2': 'val_1.2', 'el_1.3': 'val_1.3'},
+#  'el_2': {'el_2.1': 'val_2.1', 'el_2.2': 'val_2.2', 'el_2.3': 'val_2.3'},
+#  'el_3': {'el_3.1': 'val_3.1', 'el_3.2': 'val_3.2', 'el_3.3': 'val_3.3'}}
+
+# Отображение строки:
+str_pp = '\n programming language Python\n type interpreted\n year 1991\n license free \n'
+pprint(str_pp)
+# ('\n'
+#  ' programming language Python\n'
+#  ' type interpreted\n'
+#  ' year 1991\n'
+#  ' license free \n')
