1c298a9286326068c22e55e23663e1a2bc970b2d
kodzyz <kodzyzdok@gmail.com>
2022-11-28 23:37:55 +0300

    abstractmethod (Шапкин)

--- service.py
+++ service.py
@@ -1,1 +1,25 @@
+from abc import ABC, abstractmethod
+
+
+class IPriceCar(ABC):
+    def __init__(self, model, name, price, year):
+        self.model = model
+        self.name = name
+        self.price = price
+        self.year = year
+
+    @abstractmethod
+    def get_price(self):
+        raise NotImplementedError  # если этот метод не вызван будет выводить ошибку
+
+
+class KiaCar(IPriceCar):
+
+    def info(self):
+        return {'name': self.name, 'price': self.price, 'year': self.year, 'model': self.model}
+
+
+car1 = KiaCar('ria', 'kia', 1000, 2022)
+# Can't instantiate abstract class KiaCar with abstract methods get_price
+# не реализован метод get_price
 
