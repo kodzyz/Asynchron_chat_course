2e38b50227ee3bf888a1067dc889261666d3a72f
kodzyz <kodzyzdok@gmail.com>
2022-12-04 21:50:59 +0300

    аватар в бд

--- service.py
+++ service.py
@@ -1,40 +1,42 @@
 import sqlite3 as sq
 
-cars = [
-    ('Audi', 52642),
-    ('Mercedes', 57127),
-    ('Skoda', 9000),
-    ('Volvo', 29000),
-    ('Bentley', 350000)
-]
+
+def readAva(n):
+    try:
+        with open(f"avas/{n}.png", "rb") as f:
+            return f.read()
+    except IOError as e:
+        print(e)
+        return False
+
+
+def writeAva(name, data):
+    try:
+        with open(name, "wb") as f:
+            f.write(data)
+    except IOError as e:
+        print(e)
+        return False
+
+    return True
+
 
 with sq.connect("cars.db") as con:
     con.row_factory = sq.Row  # вариантом выходных данных является словарь
     cur = con.cursor()
 
-    cur.executescript("""CREATE TABLE IF NOT EXISTS cars (
-            car_id INTEGER PRIMARY KEY AUTOINCREMENT,
-            model TEXT,
-            price INTEGER)
-    """)
+    cur.executescript("""CREATE TABLE IF NOT EXISTS users (
+        name TEXT,
+        ava BLOB, 
+        score INTEGER)
+    """)  # BLOB данные в бинарнов виде
 
-    cur.executemany("INSERT INTO cars VALUES(NULL,?, ?)", cars)
+    img = readAva(1)
+    if img:
+        binary = sq.Binary(img)
+        cur.execute("INSERT INTO users VALUES ('Николай', ?, 1000)", (binary,))
 
-    cur.execute("SELECT model, price FROM cars")
-    # получить доступ к сформированной выборке
-    # rows = cur.fetchall()  # список из кортежей
-    # print(rows)
-    # rows = cur.fetchone()  # только первая запись # ('Mercedes', 59127)
-    # print(rows)
-    # rows = cur.fetchmany(4)  # не более четырех первых записей
-    # print(rows)  # [('Mercedes', 59127), ('Skoda', 11000), ('Volvo', 31000), ('Bentley', 352000)]
-    for result in cur:  # записи в цикле
-        # print(result)
-        # <sqlite3.Row object at 0x7f445385c8f0>
-        # <sqlite3.Row object at 0x7f4453700230>
-        # <sqlite3.Row object at 0x7f445385c8f0>
-        print(result['model'], result['price'])
-        # Mercedes 59127
-        # Skoda 11000
-        # Volvo 31000
-        # Bentley 352000
+        cur.execute("SELECT ava FROM users LIMIT 1")
+        img = cur.fetchone()['ava']
+
+        writeAva("out.png", img)
