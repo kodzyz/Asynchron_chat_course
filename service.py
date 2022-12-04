1b3f74ee1871a12abd0f5cd984950af821d549d0
kodzyz <kodzyzdok@gmail.com>
2022-12-04 21:17:09 +0300

    last_row_id

--- service.py
+++ service.py
@@ -1,16 +1,6 @@
 import sqlite3 as sq
 
-cars = [
-    ('Audi', 52642),
-    ('Mercedes', 57127),
-    ('Skoda', 9000),
-    ('Volvo', 29000),
-    ('Bentley', 350000)
-]
-
-con = None
-try:
-    con = sq.connect("cars.db")
+with sq.connect("cars.db") as con:
     cur = con.cursor()
 
     cur.executescript("""CREATE TABLE IF NOT EXISTS cars (
@@ -18,18 +8,11 @@ try:
             model TEXT,
             price INTEGER
         );
-        BEGIN;
-        INSERT INTO cars VALUES(NULL,'Audi',52642);
-        INSERT INTO cars VALUES(NULL,'Mercedes',57127);
-        INSERT INTO cars VALUES(NULL,'Skoda',9000);
-        INSERT INTO cars VALUES(NULL,'Volvo',29000);
-        INSERT INTO cars VALUES(NULL,'Bentley',350000);
-        UPDATE cars SET price = price+1000
-    """)  # BEGIN - метка для отката rollback()
-    con.commit()  # сохраняет все изменения
+        CREATE TABLE IF NOT EXISTS cust(name TEXT, tr_in INTEGER, buy INTEGER);        
+    """)
 
-except sq.Error as e:
-    if con: con.rollback()  # откат бд
-    print("Ошибка выполнения запроса")
-finally:
-    if con: con.close()
+    cur.execute("INSERT INTO cars VALUES(NULL,'Запорожец', 1000)")
+    last_row_id = cur.lastrowid  # rowid последней добавленной записи
+    buy_car_id = 2
+
+    cur.execute("INSERT INTO cust VALUES('Федор', ?, ?)", (last_row_id, buy_car_id))
