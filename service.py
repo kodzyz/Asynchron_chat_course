c2b580a928ce6d1529edd15dacf0f00d89dc4642
kodzyz <kodzyzdok@gmail.com>
2022-12-04 22:56:34 +0300

    clear

--- service.py
+++ service.py
@@ -1,15 +1,1 @@
-import sqlite3 as sq
 
-data = [("car", "машина"), ("house", "дом"), ("tree", "дерево"), ("color", "цвет")]
-
-con = sq.connect(':memory:')
-with con:
-    cur = con.cursor()
-    cur.execute("""CREATE TABLE IF NOT EXISTS dict(
-        eng TEXT, rus TEXT    
-    )""")
-
-    cur.executemany("INSERT INTO dict VALUES(?,?)", data)
-
-    cur.execute("SELECT rus FROM dict WHERE eng LIKE 'c%'")
-    print(cur.fetchall())
