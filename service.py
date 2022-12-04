424c703a3e79d20e5869b93c7871627db0c9f542
kodzyz <kodzyzdok@gmail.com>
2022-12-04 21:32:33 +0300

    fetchall, fetchmany, fetchone

--- service.py
+++ service.py
@@ -1,18 +1,34 @@
 import sqlite3 as sq
 
+cars = [
+    ('Audi', 52642),
+    ('Mercedes', 57127),
+    ('Skoda', 9000),
+    ('Volvo', 29000),
+    ('Bentley', 350000)
+]
+
 with sq.connect("cars.db") as con:
     cur = con.cursor()
 
     cur.executescript("""CREATE TABLE IF NOT EXISTS cars (
             car_id INTEGER PRIMARY KEY AUTOINCREMENT,
             model TEXT,
-            price INTEGER
-        );
-        CREATE TABLE IF NOT EXISTS cust(name TEXT, tr_in INTEGER, buy INTEGER);        
+            price INTEGER)
     """)
 
-    cur.execute("INSERT INTO cars VALUES(NULL,'Запорожец', 1000)")
-    last_row_id = cur.lastrowid  # rowid последней добавленной записи
-    buy_car_id = 2
+    cur.executemany("INSERT INTO cars VALUES(NULL,?, ?)", cars)
 
-    cur.execute("INSERT INTO cust VALUES('Федор', ?, ?)", (last_row_id, buy_car_id))
+    cur.execute("SELECT model, price FROM cars")
+    # получить доступ к сформированной выборке
+    # rows = cur.fetchall()  # список из кортежей
+    # print(rows)
+    # rows = cur.fetchone()  # только первая запись # ('Mercedes', 59127)
+    # print(rows)
+    # rows = cur.fetchmany(4)  # не более четырех первых записей
+    # print(rows)  # [('Mercedes', 59127), ('Skoda', 11000), ('Volvo', 31000), ('Bentley', 352000)]
+    for result in cur:  # записи в цикле
+        print(result)
+
+
+
